b307c3f5af759e0f78d21917f3a81cfffeb8726e
ninjerie <38001901+ninjerie@users.noreply.github.com>
2020-01-13 03:23:26 +0000

    Sprint 4 - Menu items
    Art created for the menu screen and coded. Began setting up first level.

---
+++ CubeVoid/Assets/Code/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject controls = null;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowControls()
+    {
+        if (controls != null)
+        {
+            if (controls.active == false)
+            {
+                controls.active = true;
+            }
+            else
+            {
+                controls.active = false;
+            }
+        }        
+    }
+}
