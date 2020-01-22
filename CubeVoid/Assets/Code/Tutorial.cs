875643aa8667182f94308a63f94ade6544a40764
ninjerie <38001901+ninjerie@users.noreply.github.com>
2020-01-22 21:31:48 +0000

    Sprint 8 - Tutorial text added
    Added tutorial text to show at specific points of the first level to give the user instruction on what to do. This is done with triggers.

---
+++ CubeVoid/Assets/Code/Tutorial.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject tutorialTxt1;
+    public GameObject tutorialTxt2 = null;
+    public GameObject tutorialBox;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (PlayerPrefs.GetInt("Stars Collected") != 0)
+        {
+            if (tutorialTxt2 != null)
+            {
+                tutorialTxt2.SetActive(true);
+            }
+            else
+            {
+                tutorialTxt1.SetActive(true);
+            }
+        }
+        else
+        {
+            tutorialTxt1.SetActive(true);
+        }
+
+        tutorialBox.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        tutorialTxt1.SetActive(false);
+
+        if (tutorialTxt2 != null)
+        {
+            tutorialTxt2.SetActive(false);
+        }
+
+        tutorialBox.SetActive(false);
+    }
+}
